821fc87134a94bb0e09b024d0c75c14c6c0c182d
Boyang Cai <caiboyang.private@gmail.com>
2018-03-10 12:26:46 -0800

    halfway

--- SmartUnlock/app/src/main/java/eem209as/smartunlock/MainActivity.java
+++ SmartUnlock/app/src/main/java/eem209as/smartunlock/MainActivity.java
@@ -1,35 +1,48 @@
 package eem209as.smartunlock;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private SensorManager sm;
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            switch (item.getItemId()) {
+//                case R.id.navigation_home:
+//                    mTextMessage.setText(R.string.title_home);
+//                    return true;
+//                case R.id.navigation_dashboard:
+//                    mTextMessage.setText(R.string.title_dashboard);
+//                    return true;
+//                case R.id.navigation_notifications:
+//                    mTextMessage.setText(R.string.title_notifications);
+//                    return true;
+//            }
+//            return false;
+//        }
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +51,48 @@ public class MainActivity extends AppCompatActivity {
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sm.registerListener(this, sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        sm.unregisterListener(this);
+        super.onDestroy();
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        if (Sensor.TYPE_ACCELEROMETER != event.sensor.getType()) {
+            return;
+        }
+
+        float[] values = event.values;
+        float ax = values[0];
+        float ay = values[1];
+
+        double g = Math.sqrt(ax * ax + ay * ay);
+        double cos = ay / g;
+        if (cos > 1) {
+            cos = 1;
+        } else if (cos < -1) {
+            cos = -1;
+        }
+        double rad = Math.acos(cos);
+        if (ax < 0) {
+            rad = 2 * Math.PI - rad;
+        }
+
+        int uiRot = getWindowManager().getDefaultDisplay().getRotation();
+        double uiRad = Math.PI / 2 * uiRot;
+        rad -= uiRad;
+
+    }
+
+    public void onAccuracyChanged(Sensor arg0, int arg1) {
+
     }
 
 }

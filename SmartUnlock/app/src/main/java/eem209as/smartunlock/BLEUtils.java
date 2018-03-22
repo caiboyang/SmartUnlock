3dc2c0e38e60f6f5f7d68a24f9a0d32f7b864e7a
Boyang Cai <caiboyang.private@gmail.com>
2018-03-21 18:20:55 -0700

    ble finished

--- SmartUnlock/app/src/main/java/eem209as/smartunlock/BLEUtils.java
+++ SmartUnlock/app/src/main/java/eem209as/smartunlock/BLEUtils.java
@@ -3,12 +3,10 @@ package eem209as.smartunlock;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothProfile;
-import android.content.Context;
 import android.util.Log;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -18,67 +16,36 @@ import java.util.Set;
 
 public class BLEUtils {
 
-    public static String getDeviceList(Context context){
-        StringBuilder res = new StringBuilder();
-        BluetoothAdapter mBlurAdapter= BluetoothAdapter.getDefaultAdapter();
-        BluetoothManager mBlurManager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
-        List<BluetoothDevice> connectedDevices = mBlurManager.getConnectedDevices(BluetoothProfile.GATT);
-        Set<BluetoothDevice> pairedDevices = mBlurAdapter.getBondedDevices();
-        if (pairedDevices.isEmpty()) {
-            Log.e("DeviceActivity ", "Paired Device not founds");
-            return "";
-        }
-        else if(connectedDevices.isEmpty()){
-            Log.e("DeviceActivity ", "Connected Device not founds");
-            return "";
-        }
-
-        for (BluetoothDevice devices : connectedDevices) {
-            res.append("Device: address: ").append(devices.getAddress());
-//            Log.d("DeviceActivity", "Device : address : " + devices.getAddress() + " name :"
-//                    + devices.getName());
-        }
-
-//        for (BluetoothDevice devices : pairedDevices) {
-//            Log.d("DeviceActivity", "Device : address : " + devices.getAddress() + " name :"
-//                    + devices.getName());
-//        }
-        return res.toString();
-    }
-
-    public String getNewDevice(){
+    public static List<BluetoothDevice> getDeviceList() {
+        List<BluetoothDevice> deviceList = new ArrayList<>();
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
         Class<BluetoothAdapter> bluetoothAdapterClass = BluetoothAdapter.class;//得到BluetoothAdapter的Class对象
-        try {//得到蓝牙状态的方法
+        try {
             Method method = bluetoothAdapterClass.getDeclaredMethod("getConnectionState", (Class[]) null);
-            //打开权限
             method.setAccessible(true);
-            int state = (int) method.invoke(_bluetoothAdapter, (Object[]) null);
+            int state = (int) method.invoke(adapter, (Object[]) null);
 
-            if(state == BluetoothAdapter.STATE_CONNECTED){
+            if (state == BluetoothAdapter.STATE_CONNECTED) {
+                Log.i("BLUETOOTH", "BluetoothAdapter.STATE_CONNECTED");
+                Set<BluetoothDevice> devices = adapter.getBondedDevices();
+                Log.i("BLUETOOTH", "devices:" + devices.size());
 
-                LogUtil.i("BluetoothAdapter.STATE_CONNECTED");
-
-                Set<BluetoothDevice> devices = _bluetoothAdapter.getBondedDevices();
-                LogUtil.i("devices:"+devices.size());
-
-                for(BluetoothDevice device : devices){
-
+                for (BluetoothDevice device : devices) {
                     Method isConnectedMethod = BluetoothDevice.class.getDeclaredMethod("isConnected", (Class[]) null);
                     method.setAccessible(true);
                     boolean isConnected = (boolean) isConnectedMethod.invoke(device, (Object[]) null);
+                    if (isConnected) {
+                        Log.i("BLUETOOTH", "connected:" + device.getName() + " address: " + device.getAddress());
+                        deviceList.add(device);
 
-                    if(isConnected){
-                        LogUtil.i("connected:"+device.getAddress());
-                        return device.getAddress();
                     }
-
                 }
-
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return deviceList;
     }
 }
 
